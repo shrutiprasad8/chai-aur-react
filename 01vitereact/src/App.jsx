4b70e33be3303c733544c3172b9be4537288a313
shrutiprasad8 <143878470+shrutiprasad8@users.noreply.github.com>
2025-03-30 23:33:21 +0530

    It is just a basic React project with Vite.

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+   <Chai />
+   <h1>chai aur react {username}</h1>  
+   <p>test para</p>   {/*evaluated expression , here we write javascript's evaluated expression*/}
+   </>
+  )
+}
+
+export default App
